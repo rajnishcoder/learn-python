3dc45b44b4f3db07fdaa811ddff93d364451cc64
rajnishcoder <getwere@gmail.com>
2017-12-18 06:31:50 +0530

    function example

--- function.py
+++ function.py
@@ -1,1 +1,16 @@
+# defining function 
 
+def functionName( parameters ):
+   "function_docstring"
+   function_suite
+   return [expression]
+
+# simple example of function  
+
+def myFunction():
+	return "Hello Python!!" 
+	pass 
+
+print myFunction()	# print function directly 
+x = myFunction()	# store function in variable in x
+print x				# print variable x
