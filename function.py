421cf8c376079e95256da22fad6c7949c619b67d
rajnishcoder <getwere@gmail.com>
2017-12-22 07:43:22 +0530

    multiple args

--- function.py
+++ function.py
@@ -1,11 +1,11 @@
-# defining function 
+######## defining function 
 
 def functionName( parameters ):
    "function_docstring"
    function_suite
    return [expression]
 
-# simple example of function  
+######### simple example of function  
 
 def myFunction():
 	return "Hello Python!!" 
@@ -15,36 +15,45 @@ print myFunction()	# print function directly
 x = myFunction()	# store function in variable in x
 print x				# print variable x
 
-# creating your own function to print
+######## creating your own function to print
 def printIt( str ):
 	print str
 	pass
 
-# passing a parameter in function
+######## passing a parameter in function
 printIt("Passing this srting to print in my own function.")
 
-# Concatenating string example with parameter value
+######## Concatenating string example with parameter value
 def appendFunction( mystr ):
 	print "this is default value", mystr
   	pass  
 
 appendFunction("and this is parameter value")
 
-# Example of Required arguments
+######### Example of Required arguments
 def printme( str ):
    "This prints a passed string into this function"
    print str
-   return;
+   pass
 
 # Now you can call printme function
 printme() # this will through error // TypeError: printme() takes exactly 1 argument (0 given)
 printme("But not this") # this will print // But not this  
 
-# Example of Keyword arguments
+######### Example of Keyword arguments
 
 def keyArg( str ):
    print str
-   return;
+   pass
 
 # Now you can call keyArg function
 keyArg( str = "My string")
+
+# you can pass multiple args 
+def multiKeyArg( name, age ):
+   print "Name: ", name
+   print "Age ", age
+   pass
+
+# Now you can call multiKeyArg function
+multiKeyArg( age=22, name="Rajnish" )
