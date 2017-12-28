c6703932ca1bec0981954bab563d2b7d273779ab
rajnishcoder <getwere@gmail.com>
2017-12-28 07:56:56 +0530

    loops (while loops)

---
+++ loops.py
@@ -0,0 +1,29 @@
+######## while loops
+
+count = 0 # count is 0
+while (count < 9): # check if count is lower than 9
+   print 'The count is:', count  # print count
+   count = count + 1  # add 1 in count
+  # Repeat
+print "Good bye!" # print gooy bye! after finishing the while loop
+
+# the upper while loop will print the count from 0 to 8
+
+########The Infinite Loop
+
+var = 1
+while var == 1 :  # This constructs an infinite loop, because var will always be 1 :)
+   num = raw_input("Enter a number  :")  # show the input popup when it runs
+   print "You entered: ", num # print the number when user enter number in input and hit enter
+
+print "Good bye!" # print gooy bye! after finishing the while loop
+
+
+########  Using else Statement with Loops
+count = 0
+while count < 5:
+   print count, " is  less than 5"
+   count = count + 1
+else:
+   print count, " is not less than 5"
+# Don't be a dumb you know it, try to understand yourself.
